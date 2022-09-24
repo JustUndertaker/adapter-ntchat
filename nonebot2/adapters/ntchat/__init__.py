7e419198932222cd9cbe58307c8d54ff7db23203
justundertaker <806792561@qq.com>
2022-09-24 18:32:17 +0800

    :art:添加快速引用

---
+++ nonebot2/adapters/ntchat/__init__.py
@@ -0,0 +1,6 @@
+from .adapter import Adapter as Adapter
+from .bot import Bot as Bot
+from .event import *
+from .message import MessageSegment as MessageSegment
+from .permission import GROUP as GROUP
+from .permission import PRIVATE as PRIVATE
