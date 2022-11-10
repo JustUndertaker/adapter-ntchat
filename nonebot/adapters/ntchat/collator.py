9bd2cdee994144e3c1f26060e99be570016cb0f4
justundertaker <806792561@qq.com>
2022-11-10 14:17:50 +0800

    :sparkles: 增加sub_wx_type

--- nonebot/adapters/ntchat/collator.py
+++ nonebot/adapters/ntchat/collator.py
@@ -1,4 +1,4 @@
-from typing import Dict, Generic, Type, TypeVar
+from typing import Dict, Generic, Tuple, Type, TypeVar
 
 from .event import Event
 
@@ -10,17 +10,23 @@ class EventModels(Generic[E]):
     事件创建器
     """
 
-    event_dict: Dict[int, Type[E]] = {}
+    event_dict: Dict[Tuple[int, int], Type[E]] = {}
     """事件模型字典"""
 
     def add_event_model(self, event: Type[E]) -> None:
         """添加事件模型"""
         event_type = event.__fields__.get("type").default
+        sub_type = event.__fields__.get("wx_sub_type")
+        if sub_type is None:
+            sub_type = 0
+        else:
+            sub_type = sub_type.default
         if event_type:
-            self.event_dict[event_type] = event
+            self.event_dict[(event_type, sub_type)] = event
 
     def get_event_model(self, data: Dict) -> Type[E]:
         """获取事件模型"""
         event_type: int = data.get("type")
-        event_model = self.event_dict.get(event_type, None)
+        sub_type = data.get("wx_sub_type", 0)
+        event_model = self.event_dict.get((event_type, sub_type), None)
         return event_model if event_model else Event
