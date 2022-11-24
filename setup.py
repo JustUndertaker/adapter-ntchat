23bef33575081b2edc0041da70e14c088634a420
justundertaker <806792561@qq.com>
2022-11-24 12:28:44 +0800

    :bookmark: 升级版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot-adapter-ntchat",
-    version="0.3.3",
+    version="0.3.4",
     author="JustUndertaker",
     author_email="806792561@qq.com",
     description="a wechat adapter for nonebot2",
